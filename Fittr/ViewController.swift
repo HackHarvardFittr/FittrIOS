2b11fdc67d53f4ca2b8df277af48af4d1911aafb
adityamaru27 <a2maru@uwaterloo.ca>
2016-10-21 22:06:45 -0400

    Initial Commit

---
+++ Fittr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fittr
+//
+//  Created by Aditya Maru on 2016-10-21.
+//  Copyright © 2016 Aditya Maru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
