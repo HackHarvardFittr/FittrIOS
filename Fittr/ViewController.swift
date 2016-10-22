19f56110eb30a22ad6154ce4c304c8ea40fa1a20
adityamaru27 <a2maru@uwaterloo.ca>
2016-10-22 10:19:54 -0400

    profile page complete

--- Fittr/ViewController.swift
+++ Fittr/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIPickerViewDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+   
+    @IBOutlet weak var sliderOutlet: UISlider!
+    
+    @IBOutlet weak var sliderAge: UILabel!
+    @IBAction func slider(_ sender: AnyObject) {
+        sliderAge.text = String(Int(sliderOutlet.value)) + " lbs"
+    }
+    
+    public var workoutArray = [String]()
+    public let userDefaults = UserDefaults.standard;
+
     @IBOutlet weak var profilePicture: UIImageView!
 
     @IBAction func selectPic(_ sender: AnyObject) {
@@ -17,7 +28,22 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 
     }
     @IBAction func stripeButton(_ sender: AnyObject) {
+        var parameter:Parameters = [
+            "name" : nameInput.text!,
+            "gymAddress" : gymInput.text!,
+            "favouriteWorkout" : workoutArray[pickerMenu.selectedRow(inComponent: 0)],
+            "weight" : sliderAge.text!
+            ];
+        var url = "http://35.161.109.99:4900/submitprofile"
+        let headers = [
+            "Content-Type" : "application/x-www-form-urlencoded"
+        ]
+        //
+        Alamofire.request(url, method: .post, parameters: parameter, encoding: URLEncoding.default, headers: headers) .response { (response) in
+            print(response)
         }
+        
+    }
     @IBOutlet weak var gymInput: UITextField!
     @IBOutlet weak var pickerMenu: UIPickerView!
     @IBOutlet weak var nameInput: UITextField!
@@ -28,12 +54,72 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
         profilePicture.clipsToBounds = true;
         profilePicture.layer.borderWidth = 1.0;
-        profilePicture.layer.masksToBounds = false
+        profilePicture.layer.masksToBounds = true
         profilePicture.layer.borderColor = UIColor.white.cgColor
+        pickerMenu.delegate = self;
+        pickerMenu.dataSource = self;
+//        profilePicture.backgroundColor = UIColor.red
+        nameInput.delegate = self
+        gymInput.delegate = self
+        pickerMenu.delegate = self
+        sliderOutlet.value = sliderOutlet.minimumValue;
+        sliderAge.text = String(Int(sliderOutlet.value)) + " lbs"
+        title = "Profile Page"
+        
+        if(userDefaults.string(forKey: "name") != nil)
+        {
+            nameInput.text = userDefaults.string(forKey: "name");
+        }
+        
+        if(userDefaults.string(forKey: "gymAddress") != nil)
+        {
+            gymInput.text = userDefaults.string(forKey: "gymAddress");
+        }
+        
+        if(userDefaults.object(forKey: "pickerIndex") != nil)
+        {
+            pickerMenu.selectRow(userDefaults.object(forKey: "pickerIndex") as! Int, inComponent: 0, animated: true)
+        }
+        
+
+        workoutArray.append("Deadlift")
+        workoutArray.append("Bench Press")
+        workoutArray.append("Squat")
+        workoutArray.append("OH")
+        workoutArray.append("Barbell Rows")
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameInput.resignFirstResponder()
+        gymInput.resignFirstResponder()
+        userDefaults.set(nameInput.text, forKey: "name");
+        userDefaults.set(gymInput.text, forKey: "gymAddress");
+        print(pickerMenu.selectedRow(inComponent: 0));
+        userDefaults.set(pickerMenu.selectedRow(inComponent: 0), forKey: "pickerIndex")
+        return true
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return workoutArray.count;
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-        <#code#>
+        return 30;
     }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return workoutArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Selected")
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -58,14 +144,13 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
         
         if let jpegData = UIImageJPEGRepresentation(image, 80) {
-//            Alamofire.upload(jpegData, to: "35.161.109.99:4900/upload").responseJSON { response in
-//                debugPrint(response)
-//            }
             profilePicture.image = UIImage(data: jpegData)
+            profilePicture.layer.cornerRadius = (profilePicture.frame.size.width)/2
+
             DispatchQueue.global().async {
                 self.send2Server();
             }
-            profilePicture.layer.cornerRadius = (profilePicture.image?.size.width)!/2
+            
 
         }
         print("Image path is: \(imagePath)")
